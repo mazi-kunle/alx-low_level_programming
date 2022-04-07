3c3afd5637b19053568abde62c63482fdce66a8a
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-07 16:43:33 -0700

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\""
-		      " - Dora Korpar, 2015-10-19\n");
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
