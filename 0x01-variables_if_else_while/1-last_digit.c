0694c42b96764e92a9983664e02eb388b4056037
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-04-10 06:03:59 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,17 +7,13 @@
  *
  * Return: Always 0.
 */
-int main()
+int main(void)
 {
-	
 	char c;
-	for (c='a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-		
 	}
 	putchar('\n');
-
 	return 0;
-
 }
