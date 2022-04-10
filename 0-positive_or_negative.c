e9b8f0c67ea700faec2a3efc2767851eb3c02730
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-10 05:43:47 -0700

    1st commit

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - This program will assign a random number to the variable          n each time it is executed.
+ *
+ * Return: always 0.
+ *
+*/
+
