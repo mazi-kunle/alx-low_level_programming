e15e609db9c41f9513bd70a22584455754b1d3eb
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-04-10 05:56:26 -0700

    2nd

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/** 
+ * main - Write a program that prints the alphabet in lowercase, 
+ *         followed by a new line.
+ *
+ * Return: Always 0.
+*/
+int main()
+{
+	
+	char c;
+	for (c='a'; c<='z'; c++)
+	{
+		putchar(c);
+		
+	}
+	putchar('\n');
+
+	return 0;
+
+}
