ca58ed73bc05be49a698106bfe5710b0665c3f8d
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-13 17:13:10 -0700

    1st

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar followed by a new line.
+ *
+ * Return: 0 if success.
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
