58dbbcf8bca634f21ac4bd46d28221ad87f1436a
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-21 15:55:28 -0700

    ed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,18 +12,23 @@ void print_array(int *a, int n)
 	int *b = a;
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	if (n > 0)
 	{
-		b++;
-	}
-
-	while (*a != *b)
+		for (i = 0; i < n - 1; i++)
+		{
+			b++;
+		}
+		while (*a != *b)
+		{
+			printf("%d", *a);
+			printf(",");
+			printf(" ");
+			a++;
+		}
+		printf("%d", *b);
+		printf("\n");
+	} else
 	{
-		printf("%d", *a);
-		printf(",");
-		printf(" ");
-		a++;
+		printf("\n");
 	}
-	printf("%d", *b);
-	printf("\n");
 }
