d6f26a5468d0b29d1ed947c4d38a51afefd738a7
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-23 15:41:00 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp- compares two strings.
+ * @s1: parameter.
+ * @s2: parameter.
+ * Return: 0 if success, non zero otherwise.
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0') || (*s2 != '\0'))
+	{
+		if (*s1 > *s2)
+		{
+			return (15);
+		}
+		else if (*s1 < *s2)
+		{
+			return (-15);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
+}
