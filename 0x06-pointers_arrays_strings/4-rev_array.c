a8c51468d9dd06d2671d39d914daec7f302d3b66
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-23 15:57:17 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * reverse_array- reverse an array in place.
+ * @a: array
+ * @n: number of element of the array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int *start, *end, temp;
+	int i;
+
+	start, end = a;
+	for (i = 0; i < n; i++)
+	{
+		end++;
+	}
+	for (i = 0; i < (n / 2); i++)
+	{
+		temp = *start;
+		*start = *end;
+		*end = *temp;
+		end--;
+		start++;
+	}
+}
+
