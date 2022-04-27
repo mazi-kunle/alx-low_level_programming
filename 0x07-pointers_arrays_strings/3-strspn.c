1d71fcf1d3a6089e7a2004e5436a6fc8ee2fd6ed
mazi-kunle <olakunlemichael644@gmail.com>
2022-04-27 08:26:27 -0700

    4e

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int len = 0;
 	unsigned int i, j;
-	
+
 	if ((s == NULL) || (accept == NULL))
 	{
 		return (len);
 	}
-	for (i =0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j =0; accept[j] != s[i]; j++)
+		for (j = 0; accept[j] != s[i]; j++)
 		{
 			if (accept[j] == '\0')
 			{
-				return i;
+				return (i);
 			}
 		}
 	}
-	return i;
+	return (i);
 }
