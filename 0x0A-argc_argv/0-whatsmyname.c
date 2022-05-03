b288f987c1adf44d51e4e17f9650789042aaffe4
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-03 05:40:57 -0700

    45

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- a function.
+ * @argc: argument counter.
+ * @argv: argument counter.
+ * Return: int.
+ *
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- print a number.
+ *
+ * @argc: argument counter.
+ * @argv: argument vector.
+ *
+ * Return: int.
+*/
+
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
