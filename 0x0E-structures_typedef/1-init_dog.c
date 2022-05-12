cb42e6d3ed1301c8d4c76af653c18fa147f681b3
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-12 16:05:35 -0700

    4th

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @d: pointer to dog structure.
 */
 
-init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *ptr;
 
