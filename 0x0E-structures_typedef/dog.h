6126e18e8e056c50b2e3619a9e49700382f5219d
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-12 15:48:41 -0700

    1st

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FIRST_H_
+#define FIRST_H_
+
+/**
+ * struct dog- a structure for dog.
+ * @name: first member.
+ * @age: second member.
+ * @owner: third owner.
+ *
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
