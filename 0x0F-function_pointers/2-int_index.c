19533b61d547acc967b7decd5d73fa7afaeb805e
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-13 16:26:09 -0700

    1st

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index- a function that searches for an integer.
+ * @array: array to search.
+ * @size: number of elements in the array.
+ * @cmp: pointer to the function to be used to compare values.
+ * Return: int.
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(i))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,79 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add- returns the sum of a and b.
+ * @a: parameter.
+ * @b: parameter.
+ * Return: int.
+*/
+
+int op_add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+/**
+ * op_sub- returns the difference of a and b.
+ * @a: parameter.
+ * @b: parameter.
+ * Return: int.
+*/
+int op_sub(int a, int b)
+{
+	int sub;
+
+	sub = a - b;
+	return (sub);
+}
+/**
+ * op_mul- returns the product of a and b.
+ * @a: parameter.
+ * @b: parameter.
+ * Return: int.
+*/
+int op_mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+/**
+ * op_div- returns the result of the division of a by b.
+ * @a: parameter.
+ * @b: parameter.
+ * Return: int.
+*/
+int op_div(int a, int b)
+{
+	int div;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	div = a / b;
+	return (div);
+}
+/**
+ * op_mod- returns the remainder of the division of a by b.
+ * @a: parameter.
+ * @b: parameter.
+ * Return: int.
+*/
+int op_mod(int a, int b)
+{
+	int mod;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	mod = a % b;
+	return (mod);
+}
+
