dcd3c99243858fa39dbfb7b4191a293c46833f7e
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-12 18:29:43 -0700

    almost

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,17 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	char *name2, *owner2;
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	strcpy(ptr->name, name);
-	strcpy(ptr->owner, owner);
+	name2 = name;
+	owner2 = owner;
+	ptr->name = name2;
 	ptr->age = age;
+	ptr->owner = owner2;
 	return (ptr);
 }
