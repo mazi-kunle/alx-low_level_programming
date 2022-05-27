506d640b222522105ca0cd889f05a1456eaa1380
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-27 09:45:54 -0700

    edit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * before_main- a function that runs before the main function.
+ *
+*/
+
+void before_main(void) __attribute__((constructor));
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,"
+		"\nI bore my house upon my back!\n");
+}
