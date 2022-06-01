21d33cd845398116933521a327f45760022b77e2
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-01 07:34:29 -0700

    r

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bitstatus;
 
 	bitstatus = (n >> index) & 1;
-	if (bitstatus == 0 || bitstatus == 1)
+	if (index < 31)
 	{
 		return (bitstatus);
 	}
