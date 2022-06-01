fcaa5e07f60ee5f160192b3323be7ef87a9daa82
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-06-01 20:27:33 +0530

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int *temp;
 
 	temp = n;
-	temp = (1 << index) | *temp;
+	*temp = (1 << index) | *temp;
 	if (index < 31)
 	{
 		return (1);
