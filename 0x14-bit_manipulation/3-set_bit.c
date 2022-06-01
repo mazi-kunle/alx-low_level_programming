b24dad3288d38616191076968d025cb72ad367a6
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-01 07:41:07 -0700

    t

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit- a function that sets the value of a bit at
+ * a given index.
+ * @n: parameter.
+ * @index: parameter.
+ *
+ * Return: int
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int newbit;
+
+	newbit = (1 << index) | *n;
+	if (index < 31)
+	{
+		return (1);
+	}
+	return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,6 @@ int power(int num, int power);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+
 #endif
