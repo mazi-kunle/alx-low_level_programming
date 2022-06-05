feb9de5048cef2027938648ecf8a95aef4fe1a9f
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-05 04:03:55 -0700

    a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * read_textfile- a function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: file to be read.
+ * @letters: number of characters to be read.
+ *
+ * Return: number of letters it coult read and print.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t fd2;
+
+	char buf[letters + 1];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	fd2 = read(fd, buf, letters);
+	if (fd2 == -1)
+	{
+		return (0);
+	}
+	buf[fd2] = '\0';
+
+	write(STDOUT_FILENO, buf, fd2);
+	close(fd);
+	return (fd2);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stddef.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
