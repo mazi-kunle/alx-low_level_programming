4c06967f6876a2c4e1b6fc552393d1770eb018ac
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-05 05:55:12 -0700

    d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		count = strlen(count);
+		count = strlen(text_content);
 		fd2 = write(fd, text_content, count + 1);
 		if (fd2 == -1)
 		{
