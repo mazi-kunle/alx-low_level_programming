8ace11ffd6849ae636606dffde5918c6978b42ce
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-06-06 00:02:54 +0530

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,13 +14,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *temp2;
 	unsigned int count;
 
-	if (head == NULL)
+	if (head == NULL || (*head == NULL && index == 0))
 	{
 		return (-1);
-	}
-	if (*head == NULL && index == 0)
-	{
-		return (1);
 	}
 	temp = *head;
 	count = 0;
