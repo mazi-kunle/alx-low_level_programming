e4277b7822a26f8c7916509b1bf39919971baa9d
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-17 06:13:24 -0700

    edit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,7 @@ void more_numbers(void)
 	{
 		for (a = 0; a < 15; a++)
 		{
-			_putchar(a + '0');
+			_putchar((a % 10) + '0');
 		}
 		_putchar('\n');
 		count = count + 1;
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,14 @@
 
 void print_line(int n)
 {
+	int a;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
-		for (int a = 0; a < n; a++)
+		for (a = 0; a < n; a++)
 		{
 			_putchar('_');
 		}
