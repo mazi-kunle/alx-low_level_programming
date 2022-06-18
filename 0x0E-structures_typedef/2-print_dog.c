dad14c566b54a87838e5e8b628870ec04eefe741
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-18 12:41:29 -0700

    edit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -26,6 +26,10 @@ void print_dog(struct dog *temp)
 		printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
-	}
+	} else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
