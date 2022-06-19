159664f9ba822bba20b6e9bd0b8d4e3ed82843b7
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-19 11:33:16 -0700

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,21 +10,26 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num * mult;
-
-			if (prod <= 9)
-			{
-				_putchar(' ');
-			} else
+			if (prod > 9)
 			{
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((prod % 10) + '0');
+			if (mult < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
+		_putchar('\n');
 	}
 }
