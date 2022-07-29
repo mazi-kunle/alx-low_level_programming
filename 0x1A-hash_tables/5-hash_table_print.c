33742ef6214788cab5da6c1691d465f16f14f4b2
mazi-kunle <olakunlemichael644@gmail.com>
2022-07-29 14:24:47 -0700

    e

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print- a function that prints a hash table.
+ * @ht: hash table.
+ *
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *ptr;
+	short int flag;
+	
+	flag = 0;
+	if (ht != NULL)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				ptr = ht->array[i];
+				while (ptr != NULL)
+				{
+					if (flag)
+					{
+						printf(", ");
+					}
+					printf("%s : ", ptr->key);
+					printf("%s", ptr->value);
+					if (flag == 0)
+					{
+						flag = 1;
+					}
+					ptr = ptr->next;
+				}
+			}
+		}
+		printf("}\n");
+	}
+}
