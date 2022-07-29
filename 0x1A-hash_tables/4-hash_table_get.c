d020f763383c3298c29e408829ed9d041cd4fd4f
mazi-kunle <olakunlemichael644@gmail.com>
2022-07-29 13:10:13 -0700

    edit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get- a function that retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key.
+ *
+ * Return: value.
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *ptr;
+
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index])
+	{
+		ptr = ht->array[index];
+		while (ptr != NULL)
+		{
+			if (strcmp(ptr->key, key) == 0)
+			{
+				return (ptr->value);
+			}
+			ptr++;
+		}
+		return (NULL);
+	}
+	return (NULL);
+}
