0ceae425afe61f0111881fb1f2b785f14c82db58
mazi-kunle <olakunlemichael644@gmail.com>
2022-07-30 09:56:23 -0700

    edit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr;
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
 	{
