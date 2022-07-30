07e8f6bad57b76c3f07f85032b46d038c6be3f20
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-30 10:51:34 -0700

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -25,8 +25,7 @@ void hash_table_print(const hash_table_t *ht)
 					{
 						printf(", ");
 					}
-					printf("\'%s\' : ", ptr->key);
-					printf("\'%s\'", ptr->value);
+			 printf("'%s': '%s'", ptr->key, ptr->value);
 					if (flag == 0)
 					{
 						flag = 1;
